1f4d4e589b16b82f9f91f519c1e48c66e2108f59
Newbies Webdev <78144742+nackdev2535@users.noreply.github.com>
2021-12-12 18:00:47 +0700

    upload file

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | MUGH Mahidol University</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header class="t-bar">
+        <div class="right-nav">
+            <img src="./img/logo.png" height="150px" width="150px" alt="">
+            <div class="title">
+                <h2>MUGH</h2>
+                <h6>Mahidol University Global Health</h6>
+            </div>
+        </div>
+        <div class="left-nav">
+            <div class="nav-menu">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Newsletter</a></li>
+                    <li><a href="#">Covid-19</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Contact us</a></li>
+
+                </ul>
+            </div>
+            <div class="t-cont">
+                <div class="tb-num">
+                    <strong>
+                        <p>02-441-9040-3</p>
+                    </strong>
+                    |
+                    <span>
+                        <a href="#"><i class="fab fa-facebook"></a></i>
+                        <a href="#"><i class="fab fa-linkedin"></a></i>
+                        <a href=""><i class="fab fa-google"></a></i>
+                    </span>
+
+                    |
+                    <form action="">
+                        <input type="text" placeholder="Search">
+                        <button class="btn-search"><i class="fas fa-search"></i></button>
+                        <button class="btn-search"><i class="fas fa-microphone"></i></button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </header>
+    <nav class="navbar">
+        <ul>
+            <li><a href="">Our Mission</a></li>
+            <li> <a href="">Our Partners</a></li>
+            <li><a href="">Activities</a></li>
+            <li><a href="">Download</a></li>
+            <li><a href="">Research</a></li>
+            <li><a href="">Services API</a></li>
+            <li> <a href="">
+                    <span>TH</span>
+                    <span>EN</span>
+                </a>
+            </li>
+        </ul>
+
+    </nav>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ main.js
@@ -0,0 +1,8 @@
+let modal=document.getElementById('login');
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "flex";
+    }
+  }
